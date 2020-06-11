4ea63ff6b0a1fe6cda0b0d513d6bd059cd5f589d
arifhossain123 <arifhossain42136@gmail.com>
2020-06-11 17:00:27 +0600

    Updates

---
+++ precache-manifest.2fa6f0413eaea1dc513b556dc993951c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9373460f79c969d8b73f0b7416410579",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "55983aa1ff78c288f659",
+    "url": "/react-game/static/css/main.81d6dbd6.chunk.css"
+  },
+  {
+    "revision": "9389e1156c7f3b7cb0e3",
+    "url": "/react-game/static/js/2.8330c730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game/static/js/2.8330c730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55983aa1ff78c288f659",
+    "url": "/react-game/static/js/main.75276e61.chunk.js"
+  },
+  {
+    "revision": "fd3adfe8c30dc0a52eef",
+    "url": "/react-game/static/js/runtime-main.6af8f3f7.js"
+  }
+]);
